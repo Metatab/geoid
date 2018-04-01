8af36b9e855fb34363900e81d850f1f15c6a84c2
Eric Busboom <eric@civicknowledge.com>
2018-04-01 10:17:26 -0700

    Beter handling of US region

--- geoid/_meta.py
+++ geoid/_meta.py
@@ -2,10 +2,10 @@
 
 
 __author__ = "Eric Busboom"
-__copyright__ = "Copyright (c) 2016 Civic Knowledge"
+__copyright__ = "Copyright (c) 2018 Civic Knowledge"
 __credits__ = []
 __license__ = "Revised BSD"
-__version__ = '1.0.4'
+__version__ = '1.0.5'
 __maintainer__ = "Eric Busboom"
 __email__ = 'eric@civicknowledge.com'
 __status__ = "Development"
--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ classifiers = [
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Software Development :: Libraries :: Python Modules',
 ]
 
